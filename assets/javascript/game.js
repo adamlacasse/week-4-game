5dfb6d1e60799f95d30406dda98de04503a1ac4a
Adam LaCasse <adamlacasse@AdamSurfaceBook.(none)>
2018-04-06 10:44:49 -0400

    added billy; everything working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,17 @@ $(document).ready(function(){
     var losses = 0;
     var score = 0;
 
+    var crystalsimgUrl = [
+        {src:"assets/images/billyCrystal.jpg", title:"Billy Crystal"},
+        {src:"assets/images/CrystalGayle.jpg", title:"Crystal Gayle"},
+        {src:"assets/images/CrystalBernard.jpg", title:"Crystal Bernard"},
+        {src:"assets/images/CrystalRenn.jpg", title:"Crystal Renn"},
+    ];
+    
     function startOver() {
         score = 0;
 
-        goal = Math.floor(Math.random() * 100 )+ 1;
+        goal = Math.floor(Math.random() * 100);
         console.log("goal: " + goal);
 
         $("#goal").html(goal);
@@ -21,33 +28,23 @@ $(document).ready(function(){
         crystalsNum = Math.floor(Math.random() * 12 )+ 1;
     }
 
-    var crystalsimgUrl =["assets/images/CrystalGayle.jpg", "assets/images/CrystalBernard.jpg", "assets/images/CrystalBowersox.jpg", "assets/images/CrystalRenn.jpg"];
-
     for (var i = 0; i < crystalsimgUrl.length; i++) {
-        var crystalsImg = $("<img>"); // creates variable crystalsImg, then adds various attributes and classes
-        crystalsImg.attr("src", crystalsimgUrl[i]); // REMEMBER THIS IS THE ONLY THING THAT IS DIFFERENT FOR EACH (other than random #)
+        var crystalsImg = $("<img>"); 
         crystalsImg.addClass("crystal-image");
+        crystalsImg.attr("src", crystalsimgUrl[i].src); 
+        crystalsImg.attr("title", crystalsimgUrl[i].title);
+        crystalsImg.attr("alt", crystalsimgUrl[i].title);
         newcrystalNum();
         crystalsImg.attr("data-crystalvalue", crystalsNum);
-        // WTF am I doing?
-            var crystalsNames = ["Crystal Gayle", "Crystal Bernard", "Crystal Bowersox", "Crystal Renn"];
-            for (var n = 0; n < 2; n++){
-            var targetName = crystalsNames[n];
-            }
-        crystalsImg.attr("title", targetName);
-
-
-        $("#crystals").append(crystalsImg); // adds the Crystals one after the other to the #crystals HTML element
-        
+        $("#crystals").append(crystalsImg);        
     }
 
     startOver();
 
-    $(".crystal-image").on("click", function() { // ok
+    $(".crystal-image").on("click", function() { 
         var crystalValue = ($(this).attr("data-crystalvalue"));
         crystalValue = parseInt(crystalValue);
-        score	+= crystalValue;
-        console.log(score);
+        score += crystalValue;
         $("#score").html(score);
 
         if (goal === score) {
